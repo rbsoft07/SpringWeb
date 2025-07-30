3b096d23f307ddc9200b4e72bd62d41e9031eaf1
rbsoft07 <rbsoft07@gmail.com>
2025-07-30 16:44:03 -0400

    Se agrega clase e interfaces para agregar la anotacion @service y clase para la implementacion

---
+++ src/main/java/com/rbsoft/springweb/services/ISpringService.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.rbsoft.springweb.services;
+
+/**
+ * @author rbsoft07	
+ * Intefaces de prueba para implementar metodos y se pueda utilzar anotaciones con Spring
+ */
+public interface ISpringService {
+
+}
---
+++ src/main/java/com/rbsoft/springweb/services/impl/SpringServiceImpl.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.rbsoft.springweb.services.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.rbsoft.springweb.services.ISpringService;
+
+/**
+ * @author Rbsoft07	
+ * Clase que impplementa la interfaces @ISpringService 
+ */
+@Service
+public class SpringServiceImpl implements ISpringService {
+
+}
